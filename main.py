a9320f871ee3b07172249b7d1ef202cc8929a664
Elvis <834223@mystma.org>
2021-09-28 09:12:22 -0500

    Password

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+def password(p):
+    if p == "knights19":
+        return "access granted"
+
+    else:
+        return "access denied"
+
+#run function
+print(password("word"))
